d63415fbc9ce9f447f40559127b8d0b4d63f6942
craig-mcgowan <craigmcgowan15@gmail.com>
2021-11-22 23:08:57 -0500

    bug fix 2

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.delete("/playlist/:id", async (req, res) => {
 
 //Playlist Show Route & Video Index Route
 app.get("/playlist/:id/", async (req, res) => {
-  const playlist = await VidPlaylist.find(req.params.id);
+  const playlist = await VidPlaylist.findById(req.params.id);
   console.log(playlist)  
   res.json(playlist);
 });
