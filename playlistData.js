1bafa97e261bf1f3d9365fffc977a509f42b25fc
ed <cabrale234@gmail.com>
2022-11-01 18:07:36 -0400

    seeding data

---
+++ playlistData.js
@@ -0,0 +1,27 @@
+[
+  {
+    userName: "CraigMcG",
+    playlistName: "Music Videos",
+    playlistTags: "Music",
+  },
+  {
+    userName: "CraigMcG",
+    playlistName: "Funny Videos",
+    playlistTags: "Comedy",
+  },
+  {
+    userName: "CraigMcG",
+    playlistName: "Educational",
+    playlistTags: "how-to",
+  },
+  {
+    userName: "CraigMcG",
+    playlistName: "Mellow Songs",
+    playlistTags: "chill",
+  },
+  {
+    userName: "David",
+    playlistName: "Sports Highlights",
+    playlistTags: "sports",
+  },
+];
